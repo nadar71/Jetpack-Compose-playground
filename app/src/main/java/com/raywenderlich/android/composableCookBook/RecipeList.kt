c9f433f70f92fab9978965275efcfb2f76b3bf5f
simone <simone.mapelli@abenergie.it>
2022-01-27 17:03:31 +0100

    feat: card list with tab bar done

---
+++ app/src/main/java/com/raywenderlich/android/composableCookBook/RecipeList.kt
@@ -0,0 +1,19 @@
+package com.raywenderlich.android.composableCookBook
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+    fun RecipeList(recipes: List<Recipe>) {
+        LazyColumn{
+            items(recipes) { item ->
+                RecipeCard(item, Modifier.padding(16.dp))
+            }
+        }
+    }
+
