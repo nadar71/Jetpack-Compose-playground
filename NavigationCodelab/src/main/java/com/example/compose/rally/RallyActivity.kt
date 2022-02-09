1963e0dba45dfe05cf56ee54ddcf501433419745
simone <simone.mapelli@abenergie.it>
2022-02-09 13:07:50 +0100

    feat: make see all navigations

--- NavigationCodelab/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ NavigationCodelab/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -8,25 +8,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.compose.rally.data.UserData
 import com.example.compose.rally.ui.accounts.AccountsBody
+import com.example.compose.rally.ui.accounts.SingleAccountBody
 import com.example.compose.rally.ui.bills.BillsBody
 import com.example.compose.rally.ui.components.RallyTabRow
 import com.example.compose.rally.ui.overview.OverviewBody
 import com.example.compose.rally.ui.theme.RallyTheme
+
 
 /**
  * This Activity recreates part of the Rally Material Study from
@@ -45,11 +44,13 @@ class RallyActivity : ComponentActivity() {
 
 @Composable
 fun RallyApp() {
+    val accountsName = RallyScreen.Accounts.name
+
     RallyTheme {
         val allScreens = RallyScreen.values().toList()
         val navController = rememberNavController()
         val backStackEntry = navController.currentBackStackEntryAsState()
-        var currentScreen = RallyScreen.fromRoute(
+        val currentScreen = RallyScreen.fromRoute(
             backStackEntry.value?.destination?.route
         )
 
@@ -70,11 +71,35 @@ fun RallyApp() {
                 modifier = Modifier.padding(innerPadding)
             ){
                 composable(RallyScreen.Overview.name){
-                    OverviewBody()
+                    OverviewBody(
+                        onClickSeeAllAccounts =
+                        { navController.navigate(RallyScreen.Accounts.name) },
+
+                        onClickSeeAllBills =
+                        { navController.navigate(RallyScreen.Bills.name) },
+
+                        onAccountClick = {name ->
+                            navigateToSingleAccount(navController,name)}
+                    )
                 }
+
+                composable(
+                    route ="$accountsName/{name}",
+                    arguments = listOf(
+                        navArgument("name"){
+                            type = NavType.StringType
+                        }
+                    )
+                ){  entry ->
+                    val accountName = entry.arguments?.getString("name")
+                    val account = UserData.getAccount(accountName)
+                    SingleAccountBody(account = account)
+                }
+
                 composable(RallyScreen.Accounts.name){
                     AccountsBody(UserData.accounts)
                 }
+
                 composable(RallyScreen.Bills.name){
                     BillsBody(bills = UserData.bills)
                 }
@@ -87,6 +112,12 @@ fun RallyApp() {
 }
 
 
+private fun navigateToSingleAccount(
+    navController: NavController,
+    accountName: String
+){
+    navController.navigate("${RallyScreen.Accounts.name}/$accountName")
+}
 
 // -------------------------------- PREVIEWS ---------------------------------------------
 @Preview(
